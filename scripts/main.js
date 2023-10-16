e159434ed1e226bb8045377015f9bc939b61ad28
KurtSchwimmbacher <231002@virtualwindow.co.za>
2023-10-16 15:24:43 +0200

    Task 3
    add all form validation

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,10 @@
+
+
+
+let password = $("#passwordInput");
+let confirm_password = $("#confirmPasswordInput");
+
+
 $(document).ready(function(){
 
 // -----------------------------------------------------------------
@@ -6,14 +13,30 @@ $(document).ready(function(){
 
 console.log("Works")
 
-
-
+password.change(function (){
+    if(password.val() != confirm_password.val()) {
+        confirm_password.setCustomValidity("Passwords Don't Match");
+      } else {
+        confirm_password.setCustomValidity('');
+      }
+});
+confirm_password.keyup(function (){
+    if(password.val() != confirm_password.val()) {
+        confirm_password[0].setCustomValidity("Passwords Don't Match");
+      } else {
+        confirm_password[0].setCustomValidity('');
+        confirm_password.repor  
+      }
+});
 
 // -----------------------------------------------------------------
 // on sign up submit prevent default submission
 $("#signUpForm").submit(function(event){
 
+
+
     event.preventDefault();
+
 
     if(this.checkValidity() === false){
         event.stopPropagation();
@@ -26,3 +49,6 @@ $("#signUpForm").submit(function(event){
 });
 
 });
+
+
+
